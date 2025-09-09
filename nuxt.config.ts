db0a954b7b11052dd3ed5862e96873acc89894fe
JAI-KAI <good003047@gmail.com>
2025-09-09 11:39:47 +0800

    修正移除nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@pinia/nuxt',  '@nuxt/content'],
+  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@pinia/nuxt'],
   css: [
     '~/assets/css/main.css',
   ],
