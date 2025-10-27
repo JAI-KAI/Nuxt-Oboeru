2b44527d5973f3726ccd3e9ffb73205f39594273
JAI-KAI <good003047@gmail.com>
2025-10-27 21:32:02 +0800

    調整eslint設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: { void: 'never' }, // 明確要求空元素不自閉合
+      },
+    ],
+    'vue/html-indent': ['error', 2],
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@pinia/nuxt', '@nuxt/eslint'],
-  css: [
-    '~/assets/css/main.css',
-  ],
+  css: ['~/assets/css/main.css'],
   colorMode: {
     preference: 'dark',
     fallback: 'dark',
@@ -15,25 +13,30 @@ export default defineNuxtConfig({
     colorMode: true,
   },
   app: {
-    baseURL: "/", // Vercel 預設根目錄
+    baseURL: '/', // Vercel 預設根目錄
     buildAssetsDir: '/_nuxt/', // 預設
     head: {
       title: '覚エル-日本語單字學習網站',
       meta: [
-        { name: 'description', content: '背單字最方便的日語學習工具！支援 N1～N5 分類與收藏功能。' },
+        {
+          name: 'description',
+          content: '背單字最方便的日語學習工具！支援 N1～N5 分類與收藏功能。',
+        },
         { name: 'keywords', content: 'JLPT, 日語, 單字, 測驗, N1, N2, N3, N4, N5' },
         { property: 'og:title', content: '覚エル-日本語單字學習網站' },
         { property: 'og:description', content: '幫助日語學習者更有效率記憶 JLPT 單字' },
-        { property: 'og:type', content: 'website' }
+        { property: 'og:type', content: 'website' },
       ],
-    }
+    },
   },
   nitro: {
     prerender: {
-      routes: [
-        '/',
-        'about'
-      ] // 預渲染首頁
+      routes: ['/', 'about'], // 預渲染首頁
+    },
+  },
+  eslint: {
+    config: {
+      stylistic: true, // <--- 啟用 ESLint 的格式化和風格規則
     },
   },
 })
