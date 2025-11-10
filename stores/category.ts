f9341cffe000b25a0bc0b113a0e24596afb23916
JAI-KAI <good003047@gmail.com>
2025-11-10 13:17:09 +0800

    ts檔格式更新

--- stores/category.ts
+++ stores/category.ts
@@ -1,15 +1,15 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useCategoryStore = defineStore('categoryToggler', () => {
-  const category = ref<string>('all')
+	const category = ref<string>('all');
 
-  function toggle(newCate: string): void {
-    category.value = newCate
-  }
+	function toggle(newCate: string): void {
+		category.value = newCate;
+	}
 
-  function reset(): void {
-    category.value = 'all'
-  }
+	function reset(): void {
+		category.value = 'all';
+	}
 
-  return { category, toggle, reset }
-})
+	return { category, toggle, reset };
+});
